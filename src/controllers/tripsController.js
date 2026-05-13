8cacae274c4768741d0eafb466040789e887e574
Roberta Fortes <betanickf@gmail.com>
2026-05-13 19:39:34 +0200

    add date param

--- src/controllers/tripsController.js
+++ src/controllers/tripsController.js
@@ -2,10 +2,17 @@ const Trip = require('../models/trips');
 
 exports.getTrips = async (req, res) => {
   try {
-    const { departure, arrival } = req.query;
+    const { departure, arrival, date } = req.query;
     const filter = {};
     if (departure) filter.departure = new RegExp(departure, 'i');
     if (arrival) filter.arrival = new RegExp(arrival, 'i');
+    if (date) {
+      const start = new Date(date);
+      start.setHours(0, 0, 0, 0);
+      const end = new Date(date);
+      end.setHours(23, 59, 59, 999);
+      filter.date = { $gte: start, $lte: end };
+    }
 
     const trips = await Trip.find(filter);
     res.json({ result: true, trips });
@@ -13,3 +20,4 @@ exports.getTrips = async (req, res) => {
     res.status(500).json({ result: false, error: 'Internal server error' });
   }
 };
+
